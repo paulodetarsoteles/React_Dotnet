047b1dcd538e9bf96ad9bf0f57d93feb351a1b69
paulodetarsoteles <potassio@hotmail.com>
2022-05-28 09:38:27 -0300

    Ultimo Commit

--- front/pro-atividade-app/src/index.js
+++ front/pro-atividade-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootswatch/dist/darkly/bootstrap.min.css'; 
 import './index.css';
 import './App.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/bootstrap_darkly.min.css'; 
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
